9368c13dc5090ae3d91a480591dece2488aa6db8
alfatrahman <162115441+alfatrahman@users.noreply.github.com>
2025-04-28 00:38:27 +0700

    Config

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,6 +1,6 @@
 import { Sequelize } from "sequelize";
 
-const db = new Sequelize ("notes02","root","",{
+const db = new Sequelize ("notes02","root","cobadulu",{
     host: "34.45.72.91",
     dialect : "mysql",
 });
